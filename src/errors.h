21d360db7c6742311896c2e8f3788f88156ed5db
dr_saturn <dr_saturn@362961b9-fb41-0410-a664-d6250bed0063>
2011-06-17 00:58:54 +0000

    Patch to error reporting. Fix for bug 2855899.
    git-svn-id: http://pngnq.svn.sourceforge.net/svnroot/pngnq@19 362961b9-fb41-0410-a664-d6250bed0063

--- src/errors.h
+++ src/errors.h
@@ -20,9 +20,11 @@
     PNGNQ_LOG_ERR(__VA_ARGS__)\
     fflush(stderr);
 
-#define PNGNQ_WARNING(...) (fprintf(stderr,"pngnq - Warning: "));\
-    fprintf(stderr, __VA_ARGS__);\
-    PNGNQ_LOG_WARNING(__VA_ARGS__)\
-    fflush(stderr);
+#define PNGNQ_WARNING(...)                                \
+    do {                                                  \
+        fprintf(stderr, "pngnq - Warning: " __VA_ARGS__); \
+        PNGNQ_LOG_WARNING(__VA_ARGS__)                    \
+        fflush(stderr);                                   \
+    } while (0)
 
 #define PNGNQ_MESSAGE(...) {if(verbose) {fprintf(stderr,__VA_ARGS__);fflush(stderr);}}
